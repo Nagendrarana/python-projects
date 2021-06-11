df523af0d75bead41c0045a620daa87ed89f004f
Nagendra Rana <62142246+Nagendrarana@users.noreply.github.com>
2021-06-11 19:57:09 +0530

    Create basic_Calculator.py

---
+++ basic_Calculator.py
@@ -0,0 +1,50 @@
+from art import logo
+print(logo)
+def add(a,b):
+  return (a+b)
+def sub(a,b):
+  return (a-b)
+def mult(a,b):
+  return (a*b)
+def div(a,b):
+  return (a/b)
+operations = {
+  "+":add,
+  "-":sub,
+  "*":mult,
+  "/":div
+}
+
+num1=int(input("Enter the first number :"))
+print("+\n-\n*\n/")
+operator=input("Enter the operation :")
+num2=int(input("Enter the secont number :"))  
+
+cal_function = operations[operator]
+answer = cal_function(num1,num2)
+print(answer)
+
+flag=False
+
+while 1:
+  step=input("press 'no' to exit \n For new calculation press 'n'\n for doing calculation in older answer press 'o' ")
+  if step=="no":
+    break
+  if step == "n":
+    num1=int(input("Enter the first number :"))
+    print("+\n-\n*\n/")
+    operator=input("Enter the operation :")
+    num2=int(input("Enter the secont number :"))
+    cal_function = operations[operator]
+    answer = cal_function(num1,num2)
+    print(answer)
+  if step =="o":
+    print("+\n-\n*\n/")
+    operator=input("Enter the operator")
+    num2=int(input("Enter the number"))
+    
+    cal_function = operations[operator]
+    answer = cal_function(answer,num2)
+    print(answer)
+
+
